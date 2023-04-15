c3923a118d38c5cd95626801d3a3f5bb7a7cc50b
PaolaUribe88 <paolauribe88@gmail.com>
2023-04-15 11:02:34 -0400

    Iniciando proyecto consumo de Apis

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const consultaComida =() =>{
+     // extraer el nombre proporcionado
+     let nombreParaConsultar = document.getElementById('nombreProporcionado').value;
+    // configuramos el endpoint a consultar concatenando el endpoint original con el nombre proporcionado
+    let endpoint = ''+nombreParaConsultar;
+
+}
