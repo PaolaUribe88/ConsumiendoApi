362ab928a43849faea4e4eb61b84db6fad6b2f28
PaolaUribe88 <paolauribe88@gmail.com>
2023-04-15 12:47:36 -0400

    inicio de proyecto, se espera ver un plato

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,33 @@ const consultaComida =() =>{
      // extraer el nombre proporcionado
      let nombreParaConsultar = document.getElementById('nombreProporcionado').value;
     // configuramos el endpoint a consultar concatenando el endpoint original con el nombre proporcionado
-    let endpoint = ''+nombreParaConsultar;
+    let endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Chicken Couscous'+nombreParaConsultar;
+    fetch(endpoint)
+    .then(respuestaComida => respuestaComida.json())
+    .then (datoPlato =>{
+        console.log(datoPlato);
+        console.log(datoPlato.meals[0].strMeal);
+        
+        let nombreRecuperado = datoPlato.meals[0].strMeal;
+        let categoriaRecuperada = datoPlato.meals[0].strCategory;
+        let areaRecuperada = datoPlato.meals[0].strArea;
+        let imagenRecuperada = datoPlato.meals[0].strMealThumb;
+        let recetaRecuperada = datoPlato.meals[0].strInstructions; 
 
+        let etiquetaNombre = document.getElementById('nombreComida');
+        etiquetaNombre.innerHTML = nombreRecuperado;
+
+        let etiquetaCategoria = document.getElementById('categoriaComida');
+        etiquetaCategoria.innerHTML = categoriaRecuperada;
+        
+        let etiquetaArea = document.getElementById('areaComida');
+        etiquetaArea.innerHTML = areaRecuperada;
+
+        let etiquetaImagen = document.getElementById('imagenComida');
+        etiquetaImagen.innerHTML = imagenRecuperada;
+
+        let etiquetaReceta = document.getElementById('recetaComida');
+        etiquetaReceta.innerHTML = recetaRecuperada;
+
+    });
 }
